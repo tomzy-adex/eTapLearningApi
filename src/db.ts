4ba5ef5a7b0d711bc5fd0e0c91617da5f0775a21
tomiwaSalad <tomiwa.aderanti@saladafrica.com>
2024-09-24 15:21:07 +0100

    ssl handling

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,13 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
-  });
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+  ssl: {
+    rejectUnauthorized:
+      process.env.ENVIRONMENT === "development" ? false : true,
+  },
+});
